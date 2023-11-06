60e7f2ae8c9d45d3cbac86103f9cf2e79d164a82
Michael <muniu.dev@gmail.com>
2023-11-06 10:26:39 +0300

    Thousands separator added to results

--- index.js
+++ index.js
@@ -4,12 +4,15 @@ document.getElementById('searchButton').addEventListener('click', function() {
     fetch('https://api.coingecko.com/api/v3/coins/' + crypto)
         .then(response => response.json())
         .then(data => {
+            var usdPrice = data.market_data.current_price.usd;
+            var kshPrice = usdPrice * 150;
+            var formattedUsdPrice = usdPrice.toLocaleString('en-US');
+            var formattedKshPrice = kshPrice.toLocaleString('en-US');
             document.getElementById('info').innerHTML = `
             <h2>${data.name}</h2>
             <p><strong>Symbol:</strong> ${data.symbol}</p>
-            <p><strong>US Dollars:</strong> ${data.market_data.current_price.usd}</p>
-            <p><strong>Kenya Shillings:</strong> ${(data.market_data.current_price.usd)*150}</p>
-                
+            <p><strong>US Dollars:</strong> $ ${formattedUsdPrice}</p>
+            <p><strong>Kenya Shillings:</strong> KES ${formattedKshPrice}</p>
             `;
         })
         .catch(() => {
