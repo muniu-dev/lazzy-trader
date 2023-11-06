aa536cecf9e8142780500f362f7280777c2fa22d
Michael <muniu.dev@gmail.com>
2023-11-06 10:00:21 +0300

    Search button added and working

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-document.getElementById('search').addEventListener('input', function(e) {
-            var crypto = e.target.value;
+document.getElementById('searchButton').addEventListener('click', function() {
+    var crypto = document.getElementById('search').value;
 
-            fetch('https://api.coingecko.com/api/v3/coins/' + crypto)
-                .then(response => response.json())
-                .then(data => {
-                    document.getElementById('info').innerHTML = `
-                        <h2>${data.name}</h2>
-                        <p>Symbol: ${data.symbol}</p>
-                        <p>Current Price: $${data.market_data.current_price.usd}</p>
-                        <p>Current Price: KES ${(data.market_data.current_price.usd)*150}</p>
-                    `;
-                })
-                .catch(() => {
-                    document.getElementById('info').innerHTML = 'Crypto not found.';
-                });
+    fetch('https://api.coingecko.com/api/v3/coins/' + crypto)
+        .then(response => response.json())
+        .then(data => {
+            document.getElementById('info').innerHTML = `
+                <h2>${data.name}</h2>
+                <p>Symbol: ${data.symbol}</p>
+                <p>Current Price: $${data.market_data.current_price.usd}</p>
+                <p>Current Price: KES ${(data.market_data.current_price.usd)*150}</p>
+            `;
+        })
+        .catch(() => {
+            document.getElementById('info').innerHTML = 'Crypto not found.';
         });
+});
